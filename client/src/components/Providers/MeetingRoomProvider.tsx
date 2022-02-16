c3bd1a6d1cc18390b780ceceba69f4f31a58673a
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2022-02-17 00:16:08 +0700

    add turn servers

--- client/src/components/Providers/MeetingRoomProvider.tsx
+++ client/src/components/Providers/MeetingRoomProvider.tsx
@@ -4,10 +4,9 @@ import { useMe, useRoomSocket } from "../../hooks";
 import useCallbackRef from "../../hooks/use-callback-ref";
 import { generateEmptyMediaTrack } from "../helper";
 
-const servers = {
+const servers: RTCConfiguration = {
   iceServers: [
     {
-      // urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
       urls: [
         "stun:stun.l.google.com:19302",
         "stun:stun1.l.google.com:19302",
@@ -23,7 +22,34 @@ const servers = {
         "stun:stun.voipbuster.com",
         "stun:stun.voipstunt.com",
         "stun:stun.voxgratia.org",
+        "turn:turn01.hubl.in?transport=udp",
+        "turn:turn02.hubl.in?transport=tcp",
       ],
+    },
+    {
+      urls: "turn:numb.viagenie.ca",
+      credential: "muazkh",
+      username: "webrtc@live.com",
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=udp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=tcp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+    {
+      urls: "turn:turn.bistri.com:80",
+      credential: "homeo",
+      username: "homeo",
+    },
+    {
+      urls: "turn:turn.anyfirewall.com:443?transport=tcp",
+      credential: "webrtc",
+      username: "webrtc",
     },
   ],
   // iceCandidatePoolSize: 10,
